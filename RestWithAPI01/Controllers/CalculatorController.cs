aad0163ecee31903346000836d4a423e1fb0df86
root <lopes1josiel@gmail.com>
2019-08-26 23:03:54 -0300

    Metodo soma

---
+++ RestWithAPI01/Controllers/CalculatorController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RestWithAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CalculatorController : ControllerBase
+    {
+        private const string V = "Erro ao somar os valores firstNumber:,secondNumber:";
+
+        [HttpGet("{firstNumber}/{secondNumber}")]
+        public IActionResult Sum(string firstNumber, string secondNumber)
+        {
+            if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            {
+                var soma = (ConvertToDecimal(firstNumber) + ConvertToDecimal(secondNumber));
+                return Ok(soma.ToString());
+            }
+            return BadRequest($"Erro ao somar os valores firstNumber:{firstNumber},secondNumber:{secondNumber}");
+        }
+
+        bool IsNumeric(string number)
+        {
+            try
+            {
+                double decimalValue;
+                return double.TryParse(number, System.Globalization.NumberStyles.Any,System.Globalization.NumberFormatInfo.InvariantInfo,out decimalValue);                
+            }
+            catch (System.Exception)
+            {
+                return false;
+            }
+        }
+
+        decimal ConvertToDecimal(string number)
+        {
+            decimal decimalValue;
+            if(decimal.TryParse(number, out decimalValue)){
+                return decimalValue;
+            }
+            return 0;
+            //return Convert.ToDecimal(number);
+        }
+    }
+}
