b03a7d548528860b54247c26538a516b599dc3a0
root <lopes1josiel@gmail.com>
2019-08-27 22:55:12 -0300

    Metodo Restantes calculadora

--- RestWithAPI01/Controllers/CalculatorController.cs
+++ RestWithAPI01/Controllers/CalculatorController.cs
@@ -21,7 +21,7 @@ namespace RestWithAPI.Controllers
             return BadRequest($"Erro ao somar os valores firstNumber:{firstNumber},secondNumber:{secondNumber}");
         }
 
-           [HttpGet("Subtraction/{firstNumber}/{secondNumber}")]
+        [HttpGet("Subtraction/{firstNumber}/{secondNumber}")]
         public IActionResult Subtraction(string firstNumber, string secondNumber)
         {
             if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
@@ -32,12 +32,45 @@ namespace RestWithAPI.Controllers
             return BadRequest($"Erro ao somar os valores firstNumber:{firstNumber},secondNumber:{secondNumber}");
         }
 
+        [HttpGet("Division/{firstNumber}/{secondNumber}")]
+        public IActionResult Division(string firstNumber, string secondNumber)
+        {
+            if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            {
+                var soma = (ConvertToDecimal(firstNumber) / ConvertToDecimal(secondNumber));
+                return Ok(soma.ToString());
+            }
+            return BadRequest($"Erro ao somar os valores firstNumber:{firstNumber},secondNumber:{secondNumber}");
+        }
+
+        [HttpGet("Multiplication/{firstNumber}/{secondNumber}")]
+        public IActionResult Multiplication(string firstNumber, string secondNumber)
+        {
+            if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            {
+                var soma = (ConvertToDecimal(firstNumber) * ConvertToDecimal(secondNumber));
+                return Ok(soma.ToString());
+            }
+            return BadRequest($"Erro ao somar os valores firstNumber:{firstNumber},secondNumber:{secondNumber}");
+        }
+
+        [HttpGet("squeare-root/{firstNumber}")]
+        public IActionResult SqueareRoot(string firstNumber)
+        {
+            if (IsNumeric(firstNumber))
+            {
+                var soma = Math.Sqrt((double)ConvertToDecimal(firstNumber));
+                return Ok(soma.ToString());
+            }
+            return BadRequest($"Erro ao somar os valores firstNumber:{firstNumber}");
+        }
+
         bool IsNumeric(string number)
         {
             try
             {
                 double decimalValue;
-                return double.TryParse(number, System.Globalization.NumberStyles.Any,System.Globalization.NumberFormatInfo.InvariantInfo,out decimalValue);                
+                return double.TryParse(number, System.Globalization.NumberStyles.Any, System.Globalization.NumberFormatInfo.InvariantInfo, out decimalValue);
             }
             catch (System.Exception)
             {
@@ -48,7 +81,8 @@ namespace RestWithAPI.Controllers
         decimal ConvertToDecimal(string number)
         {
             decimal decimalValue;
-            if(decimal.TryParse(number, out decimalValue)){
+            if (decimal.TryParse(number, out decimalValue))
+            {
                 return decimalValue;
             }
             return 0;
