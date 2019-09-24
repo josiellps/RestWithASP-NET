e62e1d514f7f797d69797ece6c554f9cec6900fd
root <lopes1josiel@gmail.com>
2019-09-23 23:16:35 -0300

    ajustado dbset books

--- RestWithAPI04/Model/Base/BaseEntity.cs
+++ RestWithAPI04/Model/Base/BaseEntity.cs
@@ -2,7 +2,7 @@ using System.Runtime.Serialization;
 
 namespace RestWithAPI.Model.Base
 {
-    [DataContract]
+    //[DataContract]
     public class BaseEntity
     {
         public long Id{get;set;}
